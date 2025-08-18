66bf716d6deedbb4fcc3aaaecada1533e221cb31
Odhiambo Dormnic <ayimdomnic@outlook.com>
2025-08-18 12:38:27 +0300

    chore(fxk): node and issues

--- src/config/swagger.ts
+++
@@ -1,6 +0,0 @@
-import { readFileSync } from 'fs';
-import { join } from 'path';
-import yaml from 'js-yaml';
-
-const swaggerFile = join(__dirname, '../docs/api-spec.yaml');
-export const swaggerSpec = yaml.load(readFileSync(swaggerFile, 'utf8')) as any;
--- src/services/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './elevator-event-logging.service';
-export * from './elevator-status.service';
-export * from './elevator-log.service';
-export * from './cache.service';
-export * from './elevator-call.service';
